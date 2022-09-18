ea912d21462b7275ad8897ae0a33be3fc0ecd65d
Anmol Prasad <anmol.gpc@gmail.com>
2022-09-18 17:30:48 +0530

    Deploying login backend

--- Connection.js
+++ Connection.js
@@ -3,17 +3,11 @@ import mongoose from "mongoose";
 const Connection = async (username, password) => {
   const URL = `mongodb+srv://firevol:${password}@cluster0.dbiaj37.mongodb.net/database`;
   try {
-    await mongoose.connect(
-      URL,
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      },
-      () => {
-        console.log("Connected to Database Successfully");
-        res.json("Connected");
-      }
-    );
+    await mongoose.connect(URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connected to Database Successfully");
   } catch (error) {
     console.log("Error: ", error.message);
   }
